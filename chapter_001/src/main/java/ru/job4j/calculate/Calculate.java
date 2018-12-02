41829c5edc679108ec85c514e416d00ec2713882
Andrey Mogilevtsev <a.mogilevtsev@gmail.com>
2018-12-03 01:32:22 +0300

    1.3.1 Maven Plugins[#93864]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,16 @@
 package ru.job4j.calculate;
 
+/**
+ *Class Calculate
+ *@author A.Mogilevtsev
+ *@since 03.02.2018
+*/
 public class Calculate {
 		
+		/**
+		 Method main.
+		 *@param args - args.
+		*/
 		public static void main(String[] arqs){
 			System.out.println("Hello worls");
 		}
