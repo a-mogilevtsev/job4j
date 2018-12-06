fd38b765a4d70e433f3317c1128d85dbccacd734
Andrey Mogilevtsev <a.mogilevtsev@gmail.com>
2018-12-06 13:34:30 +0300

    5.2. Создать программу вычисляющую факториал.[#93839]

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -16,7 +16,7 @@ public class Triangle {
     }
 
     private boolean exist(double ab, double ac, double bc) {
-        return !(ab == 0 || ac == 0 || bc == 0);
+        return ab + ac > bc  && ab + bc > ac && ac + bc > ab;
     }
 
     public double area() {
